12de372b906448634996d55daf8540d36af74b0e
Ygorfnascimento <ygorferreiradonascimentosilva@gmail.com>
2025-12-12 15:59:53 -0300

    Fix: adjusted routes and fixed bugs

--- rede_social_flesh-master/appfleshi/routes.py
+++ rede_social_flesh-master/appfleshi/routes.py
@@ -9,19 +9,29 @@ import time
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('profile', user_id=current_user.id))
+
     login_form = LoginForm()
+
     if login_form.validate_on_submit():
         user = User.query.filter_by(email=login_form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, login_form.password.data):
             login_user(user)
             return redirect(url_for('profile', user_id=user.id))
+
     return render_template('homepage.html', form=login_form)
 
 @app.route('/profile/<user_id>', methods=['GET', 'POST'])
 @login_required
 def profile(user_id):
+    user = User.query.get(int(user_id))
+
+    if user is None:
+        return "Usuário não encontrado", 404
+
     if int(user_id) != int(current_user.id):
-        user = User.query.get(int(user_id))
         return render_template('profile.html', user=user, form=None)
 
     photo_form = PhotoForm()
@@ -36,7 +46,8 @@ def profile(user_id):
             filename, ext = os.path.splitext(file.filename)
             unique_name = f"{filename}_{int(time.time())}{ext}"
 
-            path = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], secure_filename(unique_name))
+            path = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
+                                secure_filename(unique_name))
             file.save(path)
 
             photo = Photo(file_name=unique_name, user_id=current_user.id)
@@ -49,7 +60,11 @@ def profile(user_id):
 
 @app.route('/createaccount', methods=['GET', 'POST'])
 def createaccount():
+    if current_user.is_authenticated:
+        return redirect(url_for('profile', user_id=current_user.id))
+
     register_form = RegisterForm()
+
     if register_form.validate_on_submit():
         password = bcrypt.generate_password_hash(register_form.password.data)
         user = User(username=register_form.username.data, password=password, email=register_form.email.data)
@@ -58,7 +73,9 @@ def createaccount():
         login_user(user, remember=True)
 
         return redirect(url_for('profile', user_id=user.id))
+
     return render_template('createaccount.html', form=register_form)
+
 
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
@@ -96,6 +113,7 @@ def delete_photo(photo_id):
         database.session.commit()
 
         return redirect(url_for('profile', user_id=photo.user_id))
+
 
 @app.route('/like/<photo_id>', methods=['POST'])
 @login_required
